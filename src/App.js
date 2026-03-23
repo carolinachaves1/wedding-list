bf03fd6221cbbeaa89d4f5b3e76fd4f8562f0dfd
Carol Chaves <carolina.amaralchaves@gmail.com>
2026-03-23 20:22:00 -0300

    novos itens

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import PresentCard from './components/PresentCard';
 
 import lixeiraImg from './assets/lixeira.jpg';
 import panelasImg from './assets/panela.jpg';
-import escovaImg from './assets/escova.jpg';
 import escadaImg from './assets/escada3.jpg';
-import rodoImg from './assets/rodo.png';
 import ferroImg from './assets/ferroPassar.jpg';
-import vassouraImg from './assets/vassoura.png';
-import vassoura2Img from './assets/vassoura2.png';
 import portaTemperosImg from './assets/temperos.png';
 import baldesImg from './assets/baldes.jpg';
 import kitVassouraRodoImg from './assets/kitVassoura.jpg';
 import salImg from './assets/saleiro.jpg'
 import panelaEletricaImg from './assets/panelaPressao.jpg';
 import microondasImg from './assets/microondas.jpg';
+import cafeteiraImg from './assets/cafeteira.jpg';
+import jogoCamaImg from './assets/jogoCama.jpg';
+import toalhasImg from './assets/toalhas.jpg';
+import cookTopImg from './assets/cooktop.jpg';
+import mesaCadeirasImg from './assets/mesaCadeiras.jpg';
+import cristaleiraImg from './assets/cristaleira.jpg';
+import rackImg from './assets/rack.jpg';
 
 function App() {
   return (
@@ -47,7 +50,15 @@ function App() {
               { name: 'Porta Temperos', price: 80, image: portaTemperosImg },  
               { name: 'Saleiro/Açucareiro', price: 50, image: salImg },  
               { name: 'Panela Elétrica', price: 300, image: panelaEletricaImg },
-              { name: 'Micro-ondas', price: 500, image: microondasImg }
+              { name: 'Micro-ondas', price: 500, image: microondasImg },
+              { name: 'Escada Três Degraus', price: 120, image: escadaImg },
+              { name: 'Cafeteira Elétrica', price: 150, image: cafeteiraImg },
+              { name: 'Jogo de Cama Casal', price: 170, image: jogoCamaImg },
+              { name: 'Toalhas de Banho - 5 peças', price: 150, image: toalhasImg },
+              { name: 'Cooktop 4 Bocas', price: 2000, image: cookTopImg },
+              { name: 'Mesa de Jantar + 4 Cadeiras', price: 1200, image: mesaCadeirasImg },
+              { name: 'Cristaleira', price: 800, image: cristaleiraImg },
+              { name: 'Rack para TV', price: 600, image: rackImg },
 
             ].sort((a, b) => a.price - b.price).map((g, i) => (
               <PresentCard key={g.name + i} name={g.name} price={g.price} image={g.image} emoji={g.emoji} />
